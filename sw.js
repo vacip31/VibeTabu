3733b2e6a581c68f22f1711f90f1d2d1457476f5
vacip31 <burakatuk333@gmail.com>
2026-06-29 02:20:47 +0300

    555

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* VibeTabu PWA Service Worker (sw.js) */
 
-const CACHE_NAME = 'vibetabu-cache-v70';
+const CACHE_NAME = 'vibetabu-cache-v71';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
